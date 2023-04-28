12ce698e386f2d460a033b33ea92a6199089c80b
Dipak <dipakgiri1200@gmail.com>
2023-04-28 22:09:16 +0545

    updated api upload

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, './public/images/')
+    cb(null, '/uploads/')
   },
   filename(req, file, cb) {
     cb(
